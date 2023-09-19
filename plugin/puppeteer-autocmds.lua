7d21cd9b6c7a896de47a604b2900f79246fad029
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-09-19 15:05:29 +0200

    fix: support `typescriptreact`, `javascriptreact`, `vue`

--- plugin/puppeteer-autocmds.lua
+++ plugin/puppeteer-autocmds.lua
@@ -1,10 +1,18 @@
+local jsLikeFiletypes = {
+	"javascript",
+	"typescript",
+	"typescriptreact",
+	"javascriptreact",
+	"vue",
+}
+
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "python", "javascript", "typescript" },
+	pattern = { "python", unpack(jsLikeFiletypes) },
 	callback = function(ctx)
 		local ft = ctx.match
 		local func
 		if ft == "python" then func = require("puppeteer").pythonFStr end
-		if ft == "javascript" or ft == "typescript" then func = require("puppeteer").templateStr end
+		if vim.tbl_contains(jsLikeFiletypes, ft) then func = require("puppeteer").templateStr end
 		vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
 			buffer = 0,
 			callback = func,
