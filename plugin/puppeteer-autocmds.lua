ece24a78b7be657ae39b95e162476065cc62b920
Kevin Witlox <kevin.witlox@upcmail.nl>
2024-01-30 22:03:15 +0100

    feat: add cmds PuppeteerToggle/Enable/Disable

--- plugin/puppeteer-autocmds.lua
+++ plugin/puppeteer-autocmds.lua
@@ -21,9 +21,30 @@ vim.api.nvim_create_autocmd("FileType", {
 	callback = function(ctx)
 		local ft = ctx.match
 		local stringTransformFunc = require("puppeteer")[supportedFiletypes[ft]]
+		local performTransform = function()
+			if vim.b.puppeteer_enabled == false then return end
+			stringTransformFunc()
+		end
+
 		vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
 			buffer = 0,
-			callback = stringTransformFunc,
+			callback = performTransform,
 		})
 	end,
+})
+
+vim.api.nvim_create_user_command("PuppeteerDisable", "lua vim.b.puppeteer_enabled = false", {
+	desc = "Enable puppeteer for the current buffer.",
+})
+vim.api.nvim_create_user_command("PuppeteerEnable", "lua vim.b.puppeteer_enabled = true", {
+	desc = "Enable puppeteer for the current buffer.",
+})
+vim.api.nvim_create_user_command("PuppeteerToggle", function()
+	if vim.b.puppeteer_enabled == nil then
+		vim.b.puppeteer_enabled = false
+	else
+		vim.b.puppeteer_enabled = not vim.b.puppeteer_enabled
+	end
+end, {
+	desc = "Toggle puppeteer for the current buffer.",
 })
