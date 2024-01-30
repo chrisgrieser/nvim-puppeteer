8b342d10ef43779dd06795b8a1910e0d68ff17bb
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-01-31 00:46:46 +0100

    improv(user-commands): add notifications

--- plugin/puppeteer-autocmds.lua
+++ plugin/puppeteer-autocmds.lua
@@ -34,18 +34,30 @@ vim.api.nvim_create_autocmd("FileType", {
 --------------------------------------------------------------------------------
 -- USER COMMANDS
 
-vim.api.nvim_create_user_command("PuppeteerDisable", "lua vim.b.puppeteer_enabled = false", {
-	desc = "Enable puppeteer for the current buffer.",
-})
-vim.api.nvim_create_user_command("PuppeteerEnable", "lua vim.b.puppeteer_enabled = true", {
-	desc = "Enable puppeteer for the current buffer.",
-})
+
+---@param mode "Enabled"|"Disabled"
+local function notify(mode)
+	vim.notify(mode .. " for current buffer.", vim.log.levels.INFO, { title = "nvim-puppeteer" })
+end
+
+vim.api.nvim_create_user_command("PuppeteerDisable", function()
+	vim.b.puppeteer_enabled = false
+	notify("Disabled")
+end, { desc = "Disable puppeteer for the current buffer" })
+
+vim.api.nvim_create_user_command("PuppeteerEnable", function()
+	vim.b.puppeteer_enabled = true
+	notify("Enabled")
+end, { desc = "Enable puppeteer for the current buffer" })
+
 vim.api.nvim_create_user_command("PuppeteerToggle", function()
-	if vim.b.puppeteer_enabled == nil then
-		vim.b.puppeteer_enabled = false
+	if vim.b.puppeteer_enabled == true or vim.b.puppeteer_enabled == nil then
+		vim.b["puppeteer_enabled"] = false
+		notify("Disabled")
 	else
-		vim.b.puppeteer_enabled = not vim.b.puppeteer_enabled
+		vim.b["puppeteer_enabled"] = true
+		notify("Enabled")
 	end
 end, {
-	desc = "Toggle puppeteer for the current buffer.",
+	desc = "Toggle puppeteer for the current buffer",
 })
